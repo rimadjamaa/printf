7b7e07c6422e73223f2a55c36509f6783c77143b
rimadjamaa <rimadjamaa7@gmail.com>
2023-10-18 14:29:06 -0700

    push changes

--- _printf.c
+++ _printf.c
@@ -1,6 +1,6 @@
 #include <stdarg.h>
+#include "main.h"
 #include <unistd.h>
-
 /**
  * _printf - Custom printf function
  * @format: Format string
@@ -12,9 +12,7 @@ int _printf(const char *format, ...)
 va_list args;
 int count = 0;
 const char *ptr;
-
 va_start(args, format);
-
 for (ptr = format; *ptr != '\0'; ptr++)
 {
 if (*ptr == '%' && *(ptr + 1) != '\0')
@@ -23,16 +21,18 @@ ptr++;
 switch (*ptr)
 {
 case 'c':
-{
-char c = va_arg(args, int);
-count += write(1, &c, 1);
+print_char(args, &count);
 break;
-}
 case 's':
-count += write(1, va_arg(args, char *), 1);
+print_string(args, &count);
 break;
 case '%':
-count += write(1, "%", 1);
+write(1, "%", 1);
+count++;
+break;
+case 'd':
+case 'i':
+print_int(args, &count);
 break;
 default:
 write(1, "%", 1);
